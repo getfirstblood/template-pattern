a351d2ac829ebcf3d6e3b3591f6986e536114bc3
gaoweiwei <getfirstblood@gmail.com>
2012-12-18 20:06:27 +0800

    Initial Commit

---
+++ template method/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  template method
+//
+//  Created by gaoweiwei on 12-12-18.
+//  Copyright (c) 2012年 gaoweiwei. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
